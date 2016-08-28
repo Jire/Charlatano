b685ad4227aa313db64a4d336e66a97aa08425c1
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-08-28 19:31:27 -0400

    Fixed dwGlobalVars engine offset, updated classids

--- src/main/kotlin/com/charlatano/game/offsets/EngineOffsets.kt
+++ src/main/kotlin/com/charlatano/game/offsets/EngineOffsets.kt
@@ -25,6 +25,6 @@ object EngineOffsets {
 	
 	val dwClientState by engineDLL(1)(161, 0[4], 243, 15, 17, 128, 0[4], 217, 70, 4, 217, 5, 0[4])
 	val dwInGame by engineDLL(2, subtract = false)(131, 185, 0[4], 6, 15, 148, 192, 195)
-	val dwGlobalVars by engineDLL(18)(0x8B, 0x0D, 0x0, 0x0, 0x0, 0x0, 0x83, 0xC4, 0x04,
+	val dwGlobalVars by engineDLL(18)(0x8B, 0x0D, 0x0[4], 0x83, 0xC4, 0x04,
 			0x8B, 0x01, 0x68, 0x0[4], 0xFF, 0x35)
 }
