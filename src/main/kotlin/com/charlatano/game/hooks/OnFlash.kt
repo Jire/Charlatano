e44a9949d912fd4032b1329371046b4b7d45ec98
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-08-28 00:50:46 -0400

    Run noflash hook every 256ms

--- src/main/kotlin/com/charlatano/game/hooks/OnFlash.kt
+++ src/main/kotlin/com/charlatano/game/hooks/OnFlash.kt
@@ -25,7 +25,7 @@ import com.charlatano.game.offsets.ClientOffsets.dwLocalPlayer
 import com.charlatano.utils.hook
 import com.charlatano.utils.uint
 
-val onFlash = hook(32) {
+val onFlash = hook(256) {
 	val myAddress = clientDLL.uint(dwLocalPlayer)
 	val flashAlpha = csgoEXE.float(myAddress + m_flFlashMaxAlpha)
 	flashAlpha > 0f
