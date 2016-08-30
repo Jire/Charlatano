f644294265259aefb0929afc0ef315d3f413d95c
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-08-29 22:41:42 -0400

    Cleaned up ESP, added bomb site location to bomb timer

--- src/main/kotlin/com/charlatano/scripts/BombTimer.kt
+++ src/main/kotlin/com/charlatano/scripts/BombTimer.kt
@@ -1,37 +1,52 @@
 /*
- * Charlatan is a premium CS:GO cheat ran on the JVM.
- * Copyright (C) 2016 Thomas Nappo, Jonathan Beaudoin
+ *    Copyright 2016 Jonathan Beaudoin
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package com.charlatano.scripts
 
-import com.charlatano.game.CSGO
+import com.charlatano.game.CSGO.ENTITY_SIZE
+import com.charlatano.game.CSGO.clientDLL
+import com.charlatano.game.CSGO.csgoEXE
+import com.charlatano.game.CSGO.engineDLL
 import com.charlatano.game.EntityType
 import com.charlatano.game.hooks.GlowIteration
-import com.charlatano.game.netvars.NetVarOffsets
+import com.charlatano.game.netvars.NetVarOffsets.flC4Blow
+import com.charlatano.game.netvars.NetVarOffsets.szLastPlaceName
+import com.charlatano.game.offsets.ClientOffsets.dwEntityList
+import com.charlatano.game.offsets.EngineOffsets.dwGlobalVars
+import com.charlatano.utils.uint
 
+var location = ""
 
 fun bombTimer() = GlowIteration {
 	if (EntityType.CPlantedC4 == EntityType.byEntityAddress(entityAddress)) {
-		val flags = CSGO.csgoEXE.float(entityAddress + NetVarOffsets.flC4Blow)
-		val timeLeft = (CSGO.engineDLL.float(6008768 + 16) - flags) * -1
+		if (location.isEmpty()) {
+			val carrierIndex = (csgoEXE.int(entityAddress + 0x148) and 0xFFF) - 1
+			val plantedBy = if (carrierIndex == 4094) -1 else clientDLL.uint(dwEntityList + (carrierIndex * ENTITY_SIZE))
+			if (plantedBy != -1L) {
+				location = csgoEXE.read(plantedBy + szLastPlaceName, 9, true)!!.getString(0)
+			}
+		}
+		val flags = csgoEXE.float(entityAddress + flC4Blow)
+		val timeLeft = (engineDLL.float(dwGlobalVars + 16) - flags) * -1
 		if (timeLeft > 0) {
 			val hasKit = false
 			val canDefuse = if (hasKit) timeLeft >= 5 else timeLeft >= 10
-			println("$timeLeft seconds, can defuse? $canDefuse")
+			println("Location: $location, $timeLeft seconds, can defuse? $canDefuse")
+		} else {
+			location = ""
 		}
 		return@GlowIteration
 	}
--- src/main/kotlin/com/charlatano/scripts/ESP.kt
+++ src/main/kotlin/com/charlatano/scripts/ESP.kt
@@ -1,24 +1,22 @@
 /*
- * Charlatan is a premium CS:GO cheat ran on the JVM.
- * Copyright (C) 2016 Thomas Nappo, Jonathan Beaudoin
+ *    Copyright 2016 Jonathan Beaudoin
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package com.charlatano.scripts
 
-import com.charlatano.game.CSGO
+import com.charlatano.game.CSGO.ENTITY_SIZE
 import com.charlatano.game.CSGO.clientDLL
 import com.charlatano.game.CSGO.csgoEXE
 import com.charlatano.game.EntityType.*
@@ -34,7 +32,7 @@ fun esp() = GlowIteration {
 	val type = byEntityAddress(entityAddress)
 	if (type == CPlantedC4 || type == CC4) {
 		val carrierIndex = (csgoEXE.int(entityAddress + 0x148) and 0xFFF) - 1
-		bombCarrier = if (carrierIndex == 4094) -1 else clientDLL.uint(dwEntityList + (carrierIndex * CSGO.ENTITY_SIZE))//TODO find a way to get glowIndex from entityIndex?
+		bombCarrier = if (carrierIndex == 4094) -1 else clientDLL.uint(dwEntityList + (carrierIndex * ENTITY_SIZE))//TODO find a way to get glowIndex from entityIndex?
 		if (type == CPlantedC4) bombCarrier = -1L
 		glow(glowAddress)
 	} else if (type == CCSPlayer) {
