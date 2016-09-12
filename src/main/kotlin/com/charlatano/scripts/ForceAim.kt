0457124e2e254ba451851182d164874ea764237c
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-09-11 21:42:14 -0400

    Added forceaim by FOV

--- src/main/kotlin/com/charlatano/scripts/ForceAim.kt
+++ src/main/kotlin/com/charlatano/scripts/ForceAim.kt
@@ -19,61 +19,76 @@
 package com.charlatano.scripts
 
 import com.charlatano.*
-import com.charlatano.game.CSGO.clientDLL
-import com.charlatano.game.CSGO.engineDLL
-import com.charlatano.game.ClientState
 import com.charlatano.game.angle
 import com.charlatano.game.entity.*
-import com.charlatano.game.offsets.ClientOffsets.dwLocalPlayer
-import com.charlatano.game.offsets.EngineOffsets.dwClientState
+import com.charlatano.game.hooks.clientState
+import com.charlatano.game.hooks.entity
+import com.charlatano.game.hooks.me
+import com.charlatano.game.hooks.players
 import com.charlatano.utils.*
 import org.jire.arrowhead.keyPressed
 
 private var target: Player = -1L
 
+private const val LOCK_FOV = 35
+
 private const val SMOOTHING_MIN = 30F
 private const val SMOOTHING_MAX = 35F
 
 fun forceAim() = every(FORCE_AIM_SMOOTHING) {
-	val pressed = keyPressed(FORCE_AIM_KEY) {
-		val me: Player = clientDLL.uint(dwLocalPlayer)
-		if (me <= 0x200 || me.dead()) return@keyPressed
-		
-		var currentTarget = target
-		if (currentTarget == -1L) {
-			currentTarget = me.target()
-			if (currentTarget == -1L) return@keyPressed
-			target = currentTarget
-			return@keyPressed
-		}
-		
-		if (target.dead()
-				|| target.dormant()
-				|| !target.spotted()
-				|| target.team() == me.team()) {
-			target = -1L
-			return@keyPressed
-		}
-		
-		
-		val bonePosition = Vector(target.bone(0xC), target.bone(0x1C), target.bone(0x2C))
-		compensateVelocity(me, target, bonePosition, SMOOTHING_MAX)
-		
-		val dest = calculateAngle(me, bonePosition)
-		
-		val clientState: ClientState = engineDLL.uint(dwClientState)
-		var currentAngle = clientState.angle() // This randomly gets weird
-		
-		currentAngle.normalize()
-		
-		dest.finalize(currentAngle, SMOOTHING_MAX)
-		
-		currentAngle = clientState.angle()
-		val delta = Vector(currentAngle.y - dest.y, currentAngle.x - dest.x, 0F)
-		
-		val dx = Math.round(delta.x / (InGameSensitivity * InGamePitch))
-		val dy = Math.round(-delta.y / (InGameSensitivity * InGameYaw))
+    val pressed = keyPressed(FORCE_AIM_KEY) {
+        val currentAngle = clientState.angle()
+
+        var currentTarget = target
+        if (currentTarget == -1L) {
+            currentTarget = findTarget(me.position(), currentAngle)
+            if (currentTarget == -1L) return@keyPressed
+            target = currentTarget
+            return@keyPressed
+        }
+
+        if (me.dead() || target.dead() || target.dormant()
+                || !target.spotted() || target.team() == me.team()) {
+            target = -1L
+            return@keyPressed
+        }
+
+        val bonePosition = Vector(target.bone(0xC), target.bone(0x1C), target.bone(0x2C))
+        compensateVelocity(me, target, bonePosition, SMOOTHING_MAX)
+
+        val dest = calculateAngle(me, bonePosition)
+        dest.finalize(currentAngle, SMOOTHING_MAX)
+
+        val delta = Vector(currentAngle.y - dest.y, currentAngle.x - dest.x, 0F)
+
+        val dx = Math.round(delta.x / (InGameSensitivity * InGamePitch))
+        val dy = Math.round(-delta.y / (InGameSensitivity * InGameYaw))
         CUser32.mouse_event(CUser32.MOUSEEVENTF_MOVE, dx, dy, null, null)
-	}
-	if (!pressed) target = -1L
+    }
+    if (!pressed) target = -1L
+}
+
+private fun findTarget(position: Angle, angle: Angle, lockFOV: Int = LOCK_FOV): Player {
+    var closestDelta = Int.MAX_VALUE
+    var closetPlayer: Player? = null
+    for (i in 0..players.size - 1) {
+        val entity = players.entity(i)
+        if (entity == me || entity.team() == me.team()) continue
+        if (me.dead() || entity.dead() || !entity.spotted() || entity.dormant()) continue
+
+        val ePos: Angle = Vector(entity.bone(0xC), entity.bone(0x1C), entity.bone(0x2C))
+        val distance = position.distanceTo(ePos)
+
+        val dest = calculateAngle(me, ePos)
+        dest.normalize()
+
+        val yawDiff = Math.abs(angle.y - dest.y)
+        val delta = Math.abs(Math.sin(Math.toRadians(yawDiff.toDouble())) * distance)
+
+        if (delta <= lockFOV && delta < closestDelta) {
+            closestDelta = delta.toInt()
+            closetPlayer = entity
+        }
+    }
+    return closetPlayer ?: -1
 }
