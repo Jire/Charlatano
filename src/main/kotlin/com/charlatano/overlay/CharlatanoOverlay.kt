65275e8478481632e9ceeaf86ad9b84845d93147
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-09-18 20:57:24 -0400

    Made Overlay resize when game resolution changes

--- src/main/kotlin/com/charlatano/overlay/CharlatanoOverlay.kt
+++ src/main/kotlin/com/charlatano/overlay/CharlatanoOverlay.kt
@@ -1,6 +1,6 @@
 /*
  * Charlatano is a premium CS:GO cheat ran on the JVM.
- * Copyright (C) 2016 - Thomas Nappo, Jonathan Beaudoin
+ * Copyright (C) 2016 Thomas Nappo, Jonathan Beaudoin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,72 +26,75 @@ import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.charlatano.game.CSGO.gameHeight
+import com.charlatano.game.CSGO.gameWidth
 import com.charlatano.utils.paused
 import it.unimi.dsi.fastutil.objects.ObjectArrayList
 import java.util.concurrent.atomic.AtomicReference
 
 object CharlatanoOverlay : ApplicationAdapter() {
-
+	
 	val batch = AtomicReference<SpriteBatch>()
 	val camera = AtomicReference<OrthographicCamera>()
 	val shapeRenderer = AtomicReference<ShapeRenderer>()
 	val textRenderer = AtomicReference<BitmapFont>()
-
+	
 	override fun create() {
-		val cam = with(OrthographicCamera(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())) {
-			setToOrtho(true, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+		val cam = with(OrthographicCamera(gameWidth.toFloat(), gameHeight.toFloat())) {
+			setToOrtho(true, gameWidth.toFloat(), gameHeight.toFloat())
 			camera.set(this)
 			this
 		}
-
+		
 		with(SpriteBatch()) {
 			projectionMatrix = cam.combined
 			batch.set(this)
 		}
-
+		
 		with(ShapeRenderer()) {
 			setAutoShapeType(true)
 			shapeRenderer.set(this)
 		}
-
+		
 		with(BitmapFont(true)) {
 			color = Color.RED
 			textRenderer.set(this)
 		}
 	}
-
+	
 	private val bodies = ObjectArrayList<CharlatanoOverlay.() -> Unit>()
-
+	
 	override fun render() {
 		Gdx.gl.glEnable(GL20.GL_BLEND)
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA)
-
+		
 		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-
+		
 		if (paused) return
-
+		
 		val batch = batch.get()!!
 		val camera = camera.get()!!
 		val shapeRenderer = shapeRenderer.get()!!
-
+		
+		camera.setToOrtho(true, gameWidth.toFloat(), gameHeight.toFloat())
 		camera.update()
 		batch.projectionMatrix = camera.combined
 		shapeRenderer.projectionMatrix = camera.combined
-
+		
 		for (x in 0..bodies.size - 1) bodies[x]()
-
+		
 		Gdx.gl.glDisable(GL20.GL_BLEND)
 	}
-
+	
 	override fun dispose() {
 		batch.get()!!.dispose()
 		shapeRenderer.get()!!.dispose()
 		textRenderer.get()!!.dispose()
 	}
-
+	
 	operator fun invoke(body: CharlatanoOverlay.() -> Unit) {
 		bodies.add(body)
 	}
-
+	
 }
