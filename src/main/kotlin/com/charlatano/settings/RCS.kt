5043da717a67d80878e18b42ce33b291ce97abc4
Jire <thomasgnappo@gmail.com>
2017-01-19 00:07:54 -0500

    Documented RCS settings

--- src/main/kotlin/com/charlatano/settings/RCS.kt
+++ src/main/kotlin/com/charlatano/settings/RCS.kt
@@ -1,4 +1,18 @@
 package com.charlatano.settings
 
-const val RCS_DURATION = 1
+/**
+ * The amount of smoothing for the recoil control aim path.
+ *
+ * This has a minimum value of 1, and is recommended to stay slightly
+ * above your full ping (which you can see with the "ping" command).
+ *
+ * For example, if you have 55 real ping, 65 is a good value.
+ *
+ * Settings this too low may result in incorrect recoil control.
+ */
 const val RCS_SMOOTHING = 66
+
+/**
+ * The duration in milliseconds at which recoil control is checked.
+ */
+const val RCS_DURATION = 1
