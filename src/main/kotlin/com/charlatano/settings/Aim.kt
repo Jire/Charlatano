1ecba6b534a75327c4e88a4b47182c721ee70cb7
Jire <thomasgnappo@gmail.com>
2017-01-19 00:02:41 -0500

    Documented aim and bhop settings

--- src/main/kotlin/com/charlatano/settings/Aim.kt
+++ src/main/kotlin/com/charlatano/settings/Aim.kt
@@ -1,26 +1,104 @@
 package com.charlatano.settings
 
-const val FORCE_AIM_KEY = 5 // 5 = mouse forward button
+///////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////// --- GENERAL --- ///////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * The key code of the force aim button.
+ *
+ * By default, this uses the mouse forward button (5).
+ */
+const val FORCE_AIM_KEY = 5
+
+/**
+ * The field of view of the aimbot, in degrees (0 to 360).
+ */
+const val AIM_FOV = 180
+
+/**
+ * The aimbot's "playback" speed, the higher the value the slower the playback.
+ *
+ * The minimum value is 1, and max must always be greater than min.
+ */
+const val AIM_SPEED_MIN = 36
+const val AIM_SPEED_MAX = 44
+
+/**
+ * The strictness, or "stickiness" of the aimbot; the higher the number, the
+ * less strict the aimbot will stick to targets.
+ *
+ * The minimum value is 1.0
+ */
+const val AIM_STRICTNESS = 2.4
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// --- PERFECT AIM --- /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Whether or not to use perfect aim, which will instantaneously snap
+ * to the aim bone once you are within the [PERFECT_AIM_FOV].
+ */
+const val PERFECT_AIM = false
+
+/**
+ * The FOV, in degrees (0 to 360) to snap for perfect aim.
+ */
+const val PERFECT_AIM_FOV = 30
+
+/**
+ * The chance, from 1% to 100% (0 to 100) for perfect aim to activate.
+ */
+const val PERFECT_AIM_CHANCE = 100
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// --- AIM ASSIST --- //////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Enables "aim assist" mode, which has no stickiness, and gives you small
+ * extra movements towards the aim bone.
+ *
+ * This setting should be used by high-level players who are experienced aimers.
+ */
+const val AIM_ASSIST_MODE = false
+
+/**
+ * The amount of strictness for the aim assist mode, with a mimimum value of 1.
+ */
+const val AIM_ASSIST_STRICTNESS = 40
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// --- MISCELLANEOUS --- ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * The duration in milliseconds at which aimbot paths are recalculated.
+ */
+const val AIM_DURATION = 1 // duration at which fovAim paths are recalculated
+
+/**
+ * The bone IDs of the respective bones for a player.
+ *
+ * The left-side number is for CS:CO, and the right-side number is for CS:GO.
+ */
 val HEAD_BONE = if (CLASSIC_OFFENSIVE) 6 else 8
 val SHOULDER_BONE = if (CLASSIC_OFFENSIVE) 5 else 7
 val BODY_BONE = if (CLASSIC_OFFENSIVE) 4 else 6
 
-const val SHIFT_TO_SHOULDER_SHOTS = 3 // amount of shots to spray before switching to shoulder bone
-const val SHIFT_TO_BODY_SHOTS = 6 // amount of shots before switching to body bone
+/**
+ * The amount of sprayed shots until the aimbot shifts to aiming at the [SHOULDER_BONE].
+ */
+const val SHIFT_TO_SHOULDER_SHOTS = 3
 
-const val AIM_FOV = 180 // field of view, in degrees (0 to 360)
-
-const val AIM_SPEED_MIN = 36 // higher = slower, minimum = 1
-const val AIM_SPEED_MAX = 44
-
-const val AIM_DURATION = 1 // duration at which fovAim paths are recalculated
-
-const val AIM_STRICTNESS = 2.4 // higher = less strict
-
-const val AIM_ASSIST_MODE = false
-const val AIM_ASSIST_STRICTNESS = 40 // higher = less strict
-
-const val PERFECT_AIM = false
-const val PERFECT_AIM_FOV = 30
-const val PERFECT_AIM_CHANCE = 100
+/**
+ * The amount of sprayed shots until the aimbot shifts to aiming at the [BODY_BONE].
+ */
+const val SHIFT_TO_BODY_SHOTS = 6
--- src/main/kotlin/com/charlatano/settings/BunnyHop.kt
+++ src/main/kotlin/com/charlatano/settings/BunnyHop.kt
@@ -2,4 +2,9 @@ package com.charlatano.settings
 
 import java.awt.event.KeyEvent
 
-const val BUNNY_HOP_KEY = KeyEvent.VK_SPACE // spacebar
+/**
+ * The hold key which activates bunny hop.
+ *
+ * By default, this is the space bar key.
+ */
+const val BUNNY_HOP_KEY = KeyEvent.VK_SPACE
