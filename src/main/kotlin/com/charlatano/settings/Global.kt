3f0c5052b9748bf77ae72aceb28b27083364d7cb
Jire <thomasgnappo@gmail.com>
2017-01-19 00:09:03 -0500

    Documented global settings

--- src/main/kotlin/com/charlatano/settings/Global.kt
+++ src/main/kotlin/com/charlatano/settings/Global.kt
@@ -1,5 +1,13 @@
 package com.charlatano.settings
 
+/**
+ * Set this to true if you're playing CS:CO (Counter-Strike: Classic Offensive).
+ */
 const val CLASSIC_OFFENSIVE = false
 
-const val FIRE_KEY = 1 // Left click
+/**
+ * The global fire key, which you use to shoot/fire your weapon.
+ *
+ * By default, this is left click (1).
+ */
+const val FIRE_KEY = 1
