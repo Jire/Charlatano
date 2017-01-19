1433af22ac1708e1f406685f241add1b726ef38b
Jire <thomasgnappo@gmail.com>
2017-01-18 23:50:07 -0500

    Documented advanced settings

--- src/main/kotlin/com/charlatano/settings/Advanced.kt
+++ src/main/kotlin/com/charlatano/settings/Advanced.kt
@@ -1,20 +1,39 @@
 package com.charlatano.settings
 
-import com.charlatano.game.CSGO
-import com.charlatano.game.offsets.ClientOffsets
+import com.charlatano.game.CSGO.clientDLL
+import com.charlatano.game.offsets.ClientOffsets.dwSensitivity
+import com.charlatano.game.offsets.ClientOffsets.dwSensitivityPtr
 import com.charlatano.utils.extensions.uint
 
+/**
+ * The amount of FPS to run the OpenGL overlay at.
+ */
 const val OPENGL_GUI_FPS = 60
 
-const val GAME_PITCH = 0.022
-const val GAME_YAW = 0.022
+/**
+ * These should be set the same as your in-game "m_pitch" and "m_yaw" values.
+ */
+const val GAME_PITCH = 0.022 // m_pitch
+const val GAME_YAW = 0.022 // m_yaw
 
-const val MAX_ENTITIES = 1024
-const val CLEANUP_TIME = 10_000 // Interval of recycling cached entities
-
+/**
+ * The value of your in-game "sensitivity" value.
+ *
+ * This should no longer need to be changed, as it is automatically read.
+ */
 val GAME_SENSITIVITY by lazy(LazyThreadSafetyMode.NONE) {
-	val pointer = CSGO.clientDLL.address + ClientOffsets.dwSensitivityPtr
-	val value = CSGO.clientDLL.uint(ClientOffsets.dwSensitivity) xor pointer
+	val pointer = clientDLL.address + dwSensitivityPtr
+	val value = clientDLL.uint(dwSensitivity) xor pointer
 	
 	java.lang.Float.intBitsToFloat(value.toInt()).toDouble()
 }
+
+/**
+ * The maximum amount of entities that can be managed by the cached list.
+ */
+const val MAX_ENTITIES = 1024
+
+/**
+ * The interval in milliseconds to recycle entities in the cached list.
+ */
+const val CLEANUP_TIME = 10_000
