4e31d0e1aaa8cfe938cf2ad97ed20f2cb3ba0b15
Jire <thomasgnappo@gmail.com>
2017-01-19 00:32:35 -0500

    Documented `Scripts.kt`

--- src/main/kotlin/com/charlatano/Scripts.kt
+++ src/main/kotlin/com/charlatano/Scripts.kt
@@ -5,6 +5,11 @@ import com.charlatano.scripts.esp.esp
 import com.charlatano.scripts.fovAim
 import com.charlatano.scripts.rcs
 
+/**
+ * Invoke the scripts you'd like to use below.
+ *
+ * If a script is commented (has a "//" before it), it is not enabled.
+ */
 internal fun scripts() {
 	bunnyHop()
 	rcs()
