c6a2978655e4667beaa03aaef74db2b0e79f040f
Jire <thomasgnappo@gmail.com>
2017-01-20 00:31:36 -0500

    0.9.1: Cleanup and script toggling

---
+++ settings/Scripts.kts
@@ -0,0 +1,34 @@
+/**
+ * Enables the bunny hop script.
+ */
+val ENABLE_BUNNY_HOP = true
+
+/**
+ * Enables the recoil control system (RCS) script.
+ */
+val ENABLE_RCS = true
+
+/**
+ * Enables the extra sensory perception (ESP) script.
+ */
+val ENABLE_ESP = true
+
+/**
+ * Enables the aim script.
+ */
+val ENABLE_AIM = true
+
+/**
+ * Enables the trigger bot script.
+ */
+val ENABLE_TRIGGER = false
+
+/**
+ * Enables the no flash script.
+ */
+val ENABLE_NO_FLASH = true
+
+/**
+ * Enables the bomb timer script.
+ */
+val ENABLE_BOMB_TIMER = false
