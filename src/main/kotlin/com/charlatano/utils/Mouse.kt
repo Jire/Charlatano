ff3c41ec3618680dd34f1e56793ce49ee893d2c2
Jire <thomasgnappo@gmail.com>
2017-01-27 19:59:26 -0500

    Allow `dwFlags` arg for `mouseMove`

--- src/main/kotlin/com/charlatano/utils/Mouse.kt
+++ src/main/kotlin/com/charlatano/utils/Mouse.kt
@@ -20,6 +20,6 @@ package com.charlatano.utils
 
 import com.charlatano.utils.natives.CUser32
 
-private const val MOUSEEVENTF_MOVE = 0x0001
+const val MOUSEEVENTF_MOVE = 0x0001
 
-fun mouseMove(dx: Int, dy: Int) = CUser32.mouse_event(MOUSEEVENTF_MOVE, dx, dy, 0, 0)
+fun mouseMove(dx: Int, dy: Int, dwFlags: Int = MOUSEEVENTF_MOVE) = CUser32.mouse_event(dwFlags, dx, dy, 0, 0)
