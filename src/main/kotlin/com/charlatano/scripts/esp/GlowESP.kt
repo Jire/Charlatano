4992865bc5a544b2c6946fa8d29376739594e8fe
Jire <thomasgnappo@gmail.com>
2017-02-04 06:10:10 -0500

    Got rid of how??!? message lol

--- src/main/kotlin/com/charlatano/scripts/esp/GlowESP.kt
+++ src/main/kotlin/com/charlatano/scripts/esp/GlowESP.kt
@@ -29,8 +29,6 @@ import com.charlatano.utils.every
 internal fun glowEsp() = every(4) {
 	if (!GLOW_ESP) return@every
 	
-	println("HOW?!?!?!?")
-	
 	entities {
 		val entity = it.entity
 		if (entity <= 0 || me == entity) return@entities
