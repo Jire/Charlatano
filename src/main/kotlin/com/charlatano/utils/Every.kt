f22952b1a8e6c2826a662809fb841bcda2f43f59
Jire <thomasgnappo@gmail.com>
2017-06-21 22:03:10 -0400

    Indentation

--- src/main/kotlin/com/charlatano/utils/Every.kt
+++ src/main/kotlin/com/charlatano/utils/Every.kt
@@ -22,7 +22,8 @@ import kotlin.concurrent.thread
 
 @Volatile var paused = false
 
-inline fun every(duration: Int, continuous: Boolean = false, crossinline body: () -> Unit) = thread {
+inline fun every(duration: Int, continuous: Boolean = false,
+                 crossinline body: () -> Unit) = thread {
 	while (!Thread.interrupted()) {
 		if (continuous || !paused) body()
 		Thread.sleep(duration.toLong())
