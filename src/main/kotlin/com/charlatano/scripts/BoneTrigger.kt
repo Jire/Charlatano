8b3e676046d089772b18a0041e1d183eaa539737
LauraSavall <45919164+LauraSavall@users.noreply.github.com>
2018-12-16 16:58:26 +0100

    Trigger Key
    Added a trigger key, which can be modify in settings/BoneTrigger.kts

--- src/main/kotlin/com/charlatano/scripts/BoneTrigger.kt
+++ src/main/kotlin/com/charlatano/scripts/BoneTrigger.kt
@@ -26,12 +26,15 @@ import com.charlatano.scripts.aim.findTarget
 import com.charlatano.settings.BONE_TRIGGER_BONE
 import com.charlatano.settings.BONE_TRIGGER_FOV
 import com.charlatano.settings.ENABLE_BONE_TRIGGER
+import com.charlatano.settings.BONE_TRIGGER_KEY
 import com.charlatano.settings.FIRE_KEY
 import com.charlatano.utils.*
+import org.jire.arrowhead.keyPressed
 import org.jire.arrowhead.keyReleased
 
 private val onBoneTriggerTarget = hook(1) {
-	if (ENABLE_BONE_TRIGGER) findTarget(me.position(), clientState.angle(), false,
+	if (ENABLE_BONE_TRIGGER && keyPressed(BONE_TRIGGER_KEY)) findTarget(me.position(), clientState.angle(), false,
+
 			BONE_TRIGGER_FOV, BONE_TRIGGER_BONE) >= 0
 	else false
 }
